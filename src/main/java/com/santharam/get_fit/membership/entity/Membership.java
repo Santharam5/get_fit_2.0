1151a32f8c34c12c842dac3f486d602585b41dbe
Santharam5 <santharam141@gmail.com>
2026-05-26 22:20:05 +0530

    added the Annotation in the Membership entity

--- src/main/java/com/santharam/get_fit/membership/entity/Membership.java
+++ src/main/java/com/santharam/get_fit/membership/entity/Membership.java
@@ -1,4 +1,15 @@
 package com.santharam.get_fit.membership.entity;
 
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "MEMBERSHIP")
 public class Membership {
 }
