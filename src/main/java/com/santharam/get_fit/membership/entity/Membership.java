76d433a2e99f0738797df550490f69051a2b3342
Santharam5 <santharam141@gmail.com>
2026-05-26 22:21:22 +0530

    added the Id field in Membership entity.

--- src/main/java/com/santharam/get_fit/membership/entity/Membership.java
+++ src/main/java/com/santharam/get_fit/membership/entity/Membership.java
@@ -1,5 +1,8 @@
 package com.santharam.get_fit.membership.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,4 +15,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name = "MEMBERSHIP")
 public class Membership {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
